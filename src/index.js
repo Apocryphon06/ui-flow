09262bbdc3f2b5b0621d9acfeca13e003b44e6ec
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-08-27 02:52:23 +0530

    final build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allreducers from "./redux/reducers"
+
+const store = createStore(
+  allreducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useContext } from "react";
 import { PathContext } from "../context/PathContext";
+import { setNameDisplayName } from "../redux/actions";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const [fname, setfname] = useState("");
+  const [dname, setdname] = useState("");
+
   // eslint-disable-next-line
   const [pname, setPname] = useContext(PathContext);
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}
-      exit={{opacity:0}}
+    <motion.div
+      
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       <h1 className="display-6">Welcome! First things first...</h1>
       <p>You can always change them later.</p>
-      <center className="justify-text-left">
+      
         <div style={{ display: "block", padding: 30 }}>
           <Form>
             <Form.Group>
-              <Form.Label className="text-left ">Full Name</Form.Label>
+              <Form.Label className="text-left" style={{textAlign:'left'}}>Full Name</Form.Label>
               <Form.Control
-                className="inp text-muted"
+                className="inp text-muted m-auto"
                 type="text"
                 placeholder="Hrithik"
+                onChange={(e)=>setfname(e.target.value)}
               />
             </Form.Group>
             <Form.Group>
               <Form.Label className="text-left">Display Name</Form.Label>
               <Form.Control
-                className="inp text-muted"
+                className="inp text-muted m-auto"
                 type="text"
                 placeholder="apocryphon06"
+                onChange={(e)=>setdname(e.target.value)}
               />
             </Form.Group>
             <br></br>
             <Button
-              onClick={() => setPname("/workspace")}
+              onClick={() => {
+                setPname("/workspace");
+                dispatch(setNameDisplayName(fname, dname));
+              }}
               variant="secondary"
               className="btn"
               type="submit"
@@ -44,7 +59,7 @@ const Home = () => {
             </Button>
           </Form>
         </div>
-      </center>
+      
     </motion.div>
   );
 };
